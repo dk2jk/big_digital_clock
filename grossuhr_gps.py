7133f06906904e58c2da5d0c8a507d63dbf55d3c
H.Schulte <50828453+dk2jk@users.noreply.github.com>
2022-08-25 10:58:20 +0200

    pwm via hardware pin 23

--- grossuhr_gps.py
+++ grossuhr_gps.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
+# 4stelliges 7segment- tankstellen display
+# fuer raspberry pi
+# display mit spi 
+'''
+1. start des 'daemon' im system mit: sudo pigpiod
+
+2. autostart in /etc/rc.local
+    python3 /home/pi/app/python/gps/grossuhr_gps.py 15 &
+    # 15 % helligkeit
+'''
 
 from timeGps2 import MyTimeGps
 from display import Display
+from pwm import Pwm      
+import sys
 
-
-# 4stelliges 7segment- tankstellen display
-# fuer raspberry pi
-
-# display mit spi / belegung der pins
-
-
+#helligkeit in prozent als ersten parameter
+# default =20 %
+try:
+    x= sys.argv[1]
+    x= int(x)
+except Exception as e:
+    print(e)
+    x=20
+helligkeit_prozent=x
 
 def blink(sec):
     if sec & 1:
@@ -20,11 +34,14 @@ def blink(sec):
         dpx = ' '
     return dpx
 
-uhr = MyTimeGps()
+pwm= Pwm(pin=23, dc=15)  # pin23, pwm dutycycle= 15 %
+pwm.set(helligkeit_prozent)   # update 240822
 display = Display(datPin=17, clkPin=27, csPin=22)
+uhr = MyTimeGps()
 while True:
     try:   
         dt=uhr.read()
+        #print ("UTC=",dt)
         dt_local= uhr.dt_to_local(dt)  #utc nach me(s)z
         #print(dt_local)
         sec=dt_local.time().second
